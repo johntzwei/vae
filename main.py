a52d751db5858433abbd97473243b6ab68af3ebd
Johnny Wei <johntzwei@yahoo.com>
2017-08-21 17:43:37 -0700

    trains slowly, something wrong with loss function

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 from keras.models import Model
 from keras.layers import Input
 from keras.layers.core import Dense, Activation, Dropout, Lambda, \
-        RepeatVector, Permute
+        Masking
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
 from keras.layers.merge import Concatenate, Add, Multiply
@@ -87,7 +87,6 @@ class AttentionLSTM(LSTM):
         x = inputs * a_t          #broadcast
         d_t = K.sum(x, axis=-2)
 
-        #d_t = self.preprocess_input(d_t)
         lstm_states = [h_tm1, c_tm1] + list(states[4:])
         h, (h, c) = super(AttentionLSTM, self).step(d_t, lstm_states)      #pass in only lstm states
         return h, [h, c, d_t]
@@ -102,6 +101,7 @@ class AttentionLSTM(LSTM):
 def Seq2SeqAttention(input_length, output_length, vocab_size, out_vocab_size, 
         encoder_hidden_dim=256, decoder_hidden_dim=256, encoder_dropout=0.5, decoder_dropout=0.5, embedding_dim=128):
     inputs = Input(shape=(input_length,))
+    x = Masking()(inputs)
     x = Embedding(input_dim=vocab_size+1, output_dim=embedding_dim, \
             input_length=input_length, mask_zero=True)(inputs)
 
@@ -113,7 +113,7 @@ def Seq2SeqAttention(input_length, output_length, vocab_size, out_vocab_size,
     encoding = Dropout(encoder_dropout)(x)          #(None, 50, 512)
     
     x = AttentionLSTM(decoder_hidden_dim, output_length=output_length, return_sequences=True, \
-            implementation=1)(encoding)
+            unroll=True, implementation=1)(encoding)
     x = Dropout(decoder_dropout)(x)                 #(None, 50, 256)
     outputs = TimeDistributed(Dense(5, activation='softmax'))(x)
     return Model(inputs=inputs, outputs=outputs)
@@ -129,13 +129,14 @@ if __name__ == '__main__':
     print('Reading train/valid data...')
     _, X_train = ptb(section='wsj_2-21', directory='data/', column=0)
     _, y_train = ptb(section='wsj_2-21', directory='data/', column=1)
-    X_train_seq, word_to_n, n_to_word = text_to_sequence(X_train, in_vocab, maxlen=20)
-    y_train_seq, _, _ = text_to_sequence(y_train, out_vocab, maxlen=20)
+    X_train, y_train = X_train[:5000], y_train[:5000]
+    X_train_seq, word_to_n, n_to_word = text_to_sequence(X_train, in_vocab, maxlen=100)
+    y_train_seq, _, _ = text_to_sequence(y_train, out_vocab, maxlen=100)
 
     _, X_valid = ptb(section='wsj_24', directory='data/', column=0)
     _, y_valid = ptb(section='wsj_24', directory='data/', column=1)
-    X_valid_seq, word_to_n, _ = text_to_sequence(X_valid, in_vocab, maxlen=20)
-    y_valid_seq, _, _ = text_to_sequence(y_valid, out_vocab, maxlen=20)
+    X_valid_seq, word_to_n, _ = text_to_sequence(X_valid, in_vocab, maxlen=100)
+    y_valid_seq, _, _ = text_to_sequence(y_valid, out_vocab, maxlen=100)
     print('Done.')
 
     print('Contains %d unique words.' % len(in_vocab))
@@ -143,13 +144,14 @@ if __name__ == '__main__':
 
     print('Building model...')
     optimizer = optimizers.RMSprop(lr=0.001)
-    model = Seq2SeqAttention(input_length=20, output_length=20, vocab_size=len(in_vocab), out_vocab_size=len(out_vocab))
-    model.compile(optimizer=optimizer, loss='categorical_crossentropy')
+    model = Seq2SeqAttention(input_length=100, output_length=100, vocab_size=len(in_vocab), out_vocab_size=len(out_vocab))
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
     plot_model(model, to_file='model.png')
     print('Done.')
 
     print('Training model...')
-    model.fit(X_train_seq, one_hot(y_train_seq), validation_data=(X_valid_seq, one_hot(y_valid_seq)), batch_size=64, epochs=1)
+    model.fit(X_train_seq, one_hot(y_train_seq), validation_data=(X_valid_seq, one_hot(y_valid_seq)), \
+            batch_size=32, epochs=1, verbose=2)
     print('Done.')
 
     print('Saving models...')
